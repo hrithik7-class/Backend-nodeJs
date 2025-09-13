c44a45c3011d84a44603a1f783b7cd4ddc87953a
hrithik7-class <kesarwaniraj11@gmail.com>
2025-09-13 14:25:47 +0530

    chnage in middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -2,7 +2,7 @@ import { ApiError } from "../utils/apiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken"
 import User from "../models/user.model.js"
-import { application } from "express";
+
 
 export const verifyJwt = asyncHandler(async (req, _, next)=>{
    try {
